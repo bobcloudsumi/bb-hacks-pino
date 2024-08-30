da7bd86ee559c4a9afc0a6fb0ca141696f05a677
rafael-57 <rafaellavor951@gmail.com>
2024-08-31 00:53:05 +0200

    Update vk_pipeline_cache.cpp

--- src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
+++ src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
@@ -64,7 +64,9 @@ const ComputePipeline* PipelineCache::GetComputePipeline() {
 }
 
 bool ShouldSkipShader(u64 shader_hash, const char* shader_type) {
-    static constexpr std::array<u64, 5> skip_hashes = {0xa509af23, 0x4ca76892, 0xa954e79d, 0x42f2a521, 0x2da7fe60};
+    static constexpr std::array<u64, 9> skip_hashes = {0xa509af23, 0x4ca76892, 0xa954e79d,
+                                                       0x42f2a521, 0x2da7fe60, 0x77d1c63,
+                                                       0xc0cbc309, 0xff7a6d7c, 0xddfbac23};
     if (std::ranges::contains(skip_hashes, shader_hash)) {
         LOG_WARNING(Render_Vulkan, "Skipped {} shader hash {:#x}.", shader_type, shader_hash);
         return true;
